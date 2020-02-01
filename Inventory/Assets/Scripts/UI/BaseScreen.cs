1bf9b6f66b6795395a3e5a94d347722dd1e4ad1f
Joep <j.a.sijtsma@gmail.com>
2020-02-01 12:14:06 +0100

    Unknown files not yet pushed
    This was probably a while ago when i started the project

---
+++ Inventory/Assets/Scripts/UI/BaseScreen.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Canvas))]
+public class BaseScreen : MonoBehaviour
+{
+    private Canvas canvas;
+
+    public Event Opened;
+    public Event Closed;
+
+    void Awake()
+    {
+        canvas = this.GetComponent<Canvas>();
+    }
+
+    void Open()
+    {
+        canvas.enabled = true;
+        Opened.Use();
+    }
+
+    void Close()
+    {
+        canvas.enabled = false;
+        Closed.Use();
+    }
+
+}
