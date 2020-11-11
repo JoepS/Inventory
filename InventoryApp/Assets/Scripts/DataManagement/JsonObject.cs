c2e0773449438d39526f3968b3c23ddd493134ab
Joep <j.a.sijtsma@gmail.com>
2020-11-11 12:02:19 +0100

    Added Serialization
    Added the ability to save an load json files

---
+++ InventoryApp/Assets/Scripts/DataManagement/JsonObject.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace DataManagement
+{
+	[System.Serializable]
+	public abstract class JsonObject
+	{
+		public abstract string PreferredLocation
+		{
+			get;
+		}
+		public abstract string ToJson();
+	}
+
+}
