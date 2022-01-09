1592adce03322cf427756c200a98b3c5e449e75e
Joep <j.a.sijtsma@gmail.com>
2022-01-09 21:49:22 +0100

    Restart of project 2022

--- InventoryApp/Assets/Scripts/Ui/Helpers/Identifier.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Identifier", menuName = "Helpers/New Identifier", order = 0)]
-public class Identifier : ScriptableObject
-{
-    public string identity;
-}
